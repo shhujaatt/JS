396614bc18aa645843230c641b039c16e24a58c7
SHUJAAT <whokilledshujaat@hotmail.com>
2025-01-28 11:21:06 +0530

    Create Stack.js

---
+++ Stack.js
@@ -0,0 +1,19 @@
+class Stack {
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+}
+
+module.exports = Stack;
